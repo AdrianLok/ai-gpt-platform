eba18bcb9df262e5900c20aeeeca615764fe765e
AdrianLok <87637495+AdrianLok@users.noreply.github.com>
2026-05-09 04:55:19 +0800

    Update image-options.ts

--- lib/image-options.ts
+++ lib/image-options.ts
@@ -1,29 +1,13 @@
 export const imageRatios = [
-  {
-    label: "1:1",
-    value: "1024x1024",
-  },
-  {
-    label: "4:3",
-    value: "1536x1024",
-  },
-  {
-    label: "3:4",
-    value: "1024x1536",
-  },
+  { label: "1:1", value: "1:1" },
+  { label: "4:3", value: "4:3" },
+  { label: "3:4", value: "3:4" },
+  { label: "16:9", value: "16:9" },
+  { label: "9:16", value: "9:16" },
 ];
 
 export const imageDetails = [
-  {
-    label: "低",
-    value: "low",
-  },
-  {
-    label: "中",
-    value: "medium",
-  },
-  {
-    label: "高",
-    value: "high",
-  },
+  { label: "低", value: "low" },
+  { label: "中", value: "medium" },
+  { label: "高", value: "high" },
 ];
