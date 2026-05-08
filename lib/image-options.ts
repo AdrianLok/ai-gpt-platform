ad87588d412cbb15f99d14976db5eec9eb283965
AdrianLok <87637495+AdrianLok@users.noreply.github.com>
2026-05-09 03:22:58 +0800

    Create image-options.ts

---
+++ lib/image-options.ts
@@ -0,0 +1,52 @@
+export const imageSizes = [
+  {
+    label: "1K",
+    value: "1024x1024",
+  },
+  {
+    label: "2K",
+    value: "1536x1536",
+  },
+  {
+    label: "4K",
+    value: "2048x2048",
+  },
+];
+
+export const imageRatios = [
+  {
+    label: "1:1",
+    value: "1024x1024",
+  },
+  {
+    label: "4:3",
+    value: "1536x1152",
+  },
+  {
+    label: "3:4",
+    value: "1152x1536",
+  },
+  {
+    label: "16:9",
+    value: "1792x1024",
+  },
+  {
+    label: "9:16",
+    value: "1024x1792",
+  },
+];
+
+export const imageDetails = [
+  {
+    label: "低",
+    value: "low",
+  },
+  {
+    label: "中",
+    value: "medium",
+  },
+  {
+    label: "高",
+    value: "high",
+  },
+];
